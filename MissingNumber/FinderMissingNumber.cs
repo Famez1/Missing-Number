be9c6c9e6afdaea70afa0e28ed2597a9884f371a
Famez1 <a.plyakin@sh.dunice.net>
2024-07-18 10:08:48 +0300

    Сделанная задача

---
+++ MissingNumber/FinderMissingNumber.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MissingNumber;
+
+public class FinderMissingNumber
+{
+    public static int FindNumber(int[] nums) 
+    {
+        int maxValue = nums.Max();
+        int minValue = nums.Min();
+
+        for (int i = minValue; i < maxValue; i++)
+        {
+            if (!nums.Contains(i))
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ MissingNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MissingNumber;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 4, 3, 5, 5, 2, 7 };
+
+        Console.WriteLine(FinderMissingNumber.FindNumber(nums));
+    }
+}
